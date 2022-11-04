40fa94f8859b2d95ca1569bc00b2b11efda4ba49
Halderpritam123 <halderpritamhalder1@gmail.com>
2022-11-04 08:27:30 -0700

    code fixed

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 function primeNo(N){
 let count=0;
    for(let i=1;i<=N;i++){
-	if(N%i){
+	if(N%i==0){
 	count++;
 	}
     }
-     if(){
+     if(count==2){
 	console.log("prime no");
 	}else{
 	console.log("Non Prime");
